fe3cb13275e56816ee851bd653da49e41ed3f50e
Ammar Askar <ammar@ammaraskar.com>
2025-11-18 00:53:16 -0500

    Start adding content

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
 import PWABadge from './PWABadge.tsx'
 import './App.css'
+import CssBaseline from '@mui/material/CssBaseline';
+import Card from '@mui/material/Card';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import { Button, CardContent, CardHeader, Tabs, TextField, Tooltip, Typography } from '@mui/material';
+import Bluetooth from '@mui/icons-material/Bluetooth';
+import NetworkWifi from '@mui/icons-material/NetworkWifi';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="degen-beacon-web logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>degen-beacon-web</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CssBaseline />
+      <ConnectCard />
       <PWABadge />
     </>
+  )
+}
+
+function ConnectCard() {
+  return (
+      <Card sx={{ padding: '1em', minWidth: '300px', maxWidth: '600px' }} variant="outlined">
+        <CardHeader title={
+          <Typography component="h1" variant="h4">
+            Connect Beacon
+          </Typography>
+        } />
+        <CardContent>
+          <Tabs>
+            <Tab icon={<NetworkWifi />} label="WiFi" />
+            <Tooltip placement="top" title="Bluetooth connection is not supported yet" arrow>
+              <span>
+                <Tab disabled icon={<Bluetooth />} label="Bluetooth" />
+              </span>
+            </Tooltip>
+          </Tabs>
+
+          <Typography align='left' sx={{margin: '1em'}} variant="body1" gutterBottom>
+            Select <i>Pair with Terminal</i> on your beacon to grab its IP address.
+            Make sure it's connected to the same WiFi network as this device.
+          </Typography>
+          <Box>
+            <TextField size='small' id="outlined-basic" label="Beacon IP Address" variant="outlined" />
+            <Button variant="contained" sx={{ marginLeft: '1em' }}>Connect</Button>
+          </Box>
+
+        </CardContent>
+      </Card>
   )
 }
 
