3edc1f3f1480de9aa33f464534ef59f29154f50f
Ammar Askar <ammar@ammaraskar.com>
2025-11-19 10:53:55 -0500

    Add UI to edit saved messages

--- src/components/SavedMessages.tsx
+++ src/components/SavedMessages.tsx
@@ -7,6 +7,9 @@ import Edit from "@mui/icons-material/Edit";
 import ListItemText from "@mui/material/ListItemText";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
 export function SavedMessages({ rpc }: { rpc: RpcInterface }) {
     const [messages, setMessages] = useState<string[]>([]);
@@ -24,19 +27,33 @@ export function SavedMessages({ rpc }: { rpc: RpcInterface }) {
             </Typography>
             <Paper elevation={1}>
                 <List>
-                    {messages.map((msg, index) => (
-                        <ListItem key={index}
-                            secondaryAction={
-                                <IconButton edge="end" aria-label="edit">
-                                    <Edit />
-                                </IconButton>
-                            }>
-                            <ListItemText primary={msg} />
-                        </ListItem>
-                    ))
-                    }
+                    {messages.map((msg, index) => <MessageItem idx={index} message={msg} key={index} />)}
                 </List>
             </Paper>
         </>
+    );
+}
+
+function MessageItem({ idx, message }: { idx: number; message: string }) {
+    const [editMode, setEditMode] = useState(false);
+
+    return (
+        <ListItem
+            secondaryAction={
+                editMode ? 
+                    null :
+                    <IconButton edge="end" aria-label="edit" onClick={() => setEditMode(!editMode)}>
+                        <Edit />
+                    </IconButton>
+            }>
+            <ListItemText
+                primary={editMode ?
+                    <Stack direction="row" spacing={1}>
+                        <TextField fullWidth size="small" value={message} />
+                        <Button variant="outlined">Save</Button>
+                        <Button variant="outlined" color="error" onClick={() => setEditMode(false)}>Cancel</Button>
+                    </Stack> :
+                    message} />
+        </ListItem>
     );
 }
